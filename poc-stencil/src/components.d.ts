8f5c1dae7ec867e0c32c866d524f305fa6e35f4d
mariana-oliveira-movile <mariana.oliveira@sinch.com>
2021-07-20 11:51:20 -0300

    feat: create input component

--- poc-stencil/src/components.d.ts
+++ poc-stencil/src/components.d.ts
@@ -20,6 +20,10 @@ export namespace Components {
          */
         "middle": string;
     }
+    interface MyInput {
+        "label": string;
+        "placeholder": string;
+    }
 }
 declare global {
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
@@ -28,8 +32,15 @@ declare global {
         prototype: HTMLMyComponentElement;
         new (): HTMLMyComponentElement;
     };
+    interface HTMLMyInputElement extends Components.MyInput, HTMLStencilElement {
+    }
+    var HTMLMyInputElement: {
+        prototype: HTMLMyInputElement;
+        new (): HTMLMyInputElement;
+    };
     interface HTMLElementTagNameMap {
         "my-component": HTMLMyComponentElement;
+        "my-input": HTMLMyInputElement;
     }
 }
 declare namespace LocalJSX {
@@ -47,8 +58,13 @@ declare namespace LocalJSX {
          */
         "middle"?: string;
     }
+    interface MyInput {
+        "label"?: string;
+        "placeholder"?: string;
+    }
     interface IntrinsicElements {
         "my-component": MyComponent;
+        "my-input": MyInput;
     }
 }
 export { LocalJSX as JSX };
@@ -56,6 +72,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "my-input": LocalJSX.MyInput & JSXBase.HTMLAttributes<HTMLMyInputElement>;
         }
     }
 }
---
+++ poc-stencil/src/components/my-input/my-input.tsx
@@ -0,0 +1,41 @@
+import { Component, Prop, State, h } from "@stencil/core";
+
+@Component({
+  tag: 'my-input',
+  styleUrl: 'my-input.css',
+})
+export class MyInput {
+
+  @Prop() label: string;
+
+  @Prop() placeholder: string;
+
+  @State() value: string;
+
+  handleChange(event) {
+    this.value = event.target.value;
+  }
+
+  render() {
+    return (
+      <div class="container">
+
+        <label htmlFor="my-input">{this.label}
+          <input 
+            type="text"
+            name="my-input"
+            autocomplete="off"
+            placeholder={this.placeholder}
+            value={this.value} onInput={(event) => this.handleChange(event)}
+          />
+        </label>
+        <div>
+          <h4>Your name is:</h4>  
+          <p>{this.value}</p>
+        </div>
+
+
+      </div>
+    )
+  }
+}
